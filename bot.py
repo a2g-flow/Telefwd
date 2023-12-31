9de79f762618143813e03afe72e17fbc029884b1
a2g-flow <gliderinkflow@gmail.com>
2024-01-01 00:15:09 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,9 @@
+from aiohttp import web
+from plugins import web_server
+
+PORT = "8080"
+
+app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
